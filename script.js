5a178e47e3aed3989aaca7c5877dd293507b6534
Steven <zhangc2@illinois.edu>
2023-12-05 21:44:05 -0600

    moved readme to root

--- script.js
+++ script.js
@@ -61,6 +61,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
+
 const cardSymbols = {
     'hearts': ['2♥', '3♥', '4♥', '5♥', '6♥', '7♥', '8♥', '9♥', '10♥', 'J♥', 'Q♥', 'K♥', 'A♥'],
     'diamonds': ['2♦', '3♦', '4♦', '5♦', '6♦', '7♦', '8♦', '9♦', '10♦', 'J♦', 'Q♦', 'K♦', 'A♦'],
